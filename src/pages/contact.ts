759365c95b92b86094a6e055ccdad7ddff6e597f
jradam <15231996+jradam@users.noreply.github.com>
2024-09-30 18:11:05 -0400

    laying out the form

--- src/pages/contact.ts
+++ src/pages/contact.ts
@@ -5,6 +5,25 @@ export default function pageContact(): void {
 
   page.innerHTML = `
 <x-title>Get in touch</x-title>
-<div class='flex justify-center py-44 font-lilita text-3xl'>james@digitalanimal.com</div>
+
+<div class='flex flex-col items-center py-44'>
+
+  Drop us a line and we'll be in touch right away!
+
+  james@digitalanimal.com
+
+  <form action="" class='flex flex-col'>
+    <label for="name">Name</label>
+    <input type="text" id="name" name="name" placeholder="Your name...">
+
+    <label for="email">Email</label>
+    <input type="text" id="email" email="email" placeholder="Your email...">
+
+    <label for="message">Message</label>
+    <textarea id="message" name="message" placeholder="Tell us about it..."></textarea>
+
+    <button type="submit" class='primary'>Send</button>
+  </form>
+</div>
 `
 }
