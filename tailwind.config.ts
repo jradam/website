bcda94efb43f7bde6c8c9d2ebd01edd34c139d38
jradam <15231996+jradam@users.noreply.github.com>
2024-10-01 10:06:12 -0400

    changing tailwind config to typescript

---
+++ tailwind.config.ts
@@ -0,0 +1,58 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+import plugin from 'tailwindcss/plugin'
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    screens: {
+      xxs: '400px',
+      xs: '475px',
+      ...defaultTheme.screens,
+    },
+    extend: {
+      fontFamily: { lilita: 'lilita', manrope: 'manrope', gochi: 'gochi' },
+      colors: { mustard: '#FECD60', clay: '#1f2937', purple: '#c084fc' },
+      keyframes: {
+        blink: {
+          '0%': { opacity: '1' },
+          '49%': { opacity: '1' },
+          '50%': { opacity: '0' },
+          '100%': { opacity: '0' },
+        },
+        wave: {
+          '0%, 100%': { transform: 'rotate(0deg)' },
+          '50%': { transform: 'rotate(10deg)' },
+        },
+      },
+      animation: {
+        blink: 'blink 1.25s infinite',
+        wave: 'wave 3s infinite',
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ addBase }) {
+      addBase({
+        // Scale everything up at larger sizes
+        html: { fontSize: '16px' },
+        '@screen xs': { html: { fontSize: '18px' } },
+        '@screen 2xl': { html: { fontSize: '20px' } },
+      })
+    }),
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.drag-none': {
+          '-webkit-user-drag': 'none',
+          '-khtml-user-drag': 'none',
+          '-moz-user-drag': 'none',
+          '-o-user-drag': 'none',
+          'user-drag': 'none',
+        },
+      })
+    }),
+  ],
+  future: {
+    hoverOnlyWhenSupported: true, // Fix buttons getting stuck down on mobile
+  },
+} satisfies Config
