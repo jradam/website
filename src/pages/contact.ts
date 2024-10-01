84577d3292b92a5494fb7f1494f8fa90879371f2
jradam <15231996+jradam@users.noreply.github.com>
2024-09-30 21:03:52 -0400

    wip styling the inputs

--- src/pages/contact.ts
+++ src/pages/contact.ts
@@ -6,21 +6,29 @@ export default function pageContact(): void {
   page.innerHTML = `
 <x-title>Get in touch</x-title>
 
-<div class='flex flex-col items-center py-44'>
+<div class='flex flex-col items-center gap-y-12 py-44'>
 
-  Drop us a line and we'll be in touch right away!
+  <div>
+    Drop us a line and we'll be in touch right away!
 
-  james@digitalanimal.com
+    james@digitalanimal.com
+  </div>
 
-  <form action="" class='flex flex-col'>
-    <label for="name">Name</label>
-    <input type="text" id="name" name="name" placeholder="Your name...">
+  <form action="" class='flex flex-col items-center gap-y-12'>
+    <div>
+      <label for="name">Name</label>
+      <input type="text" id="name" name="name" placeholder="Your name...">
+    </div>
 
-    <label for="email">Email</label>
-    <input type="text" id="email" email="email" placeholder="Your email...">
+    <div>
+      <label for="email">Email</label>
+      <input type="text" id="email" email="email" placeholder="Your email...">
+    </div>
 
-    <label for="message">Message</label>
-    <textarea id="message" name="message" placeholder="Tell us about it..."></textarea>
+    <div>
+      <label for="message">Message</label>
+      <textarea id="message" name="message" placeholder="Tell us about it..."></textarea>
+    </div>
 
     <button type="submit" class='primary'>Send</button>
   </form>
