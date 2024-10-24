9f5f1f225ef695d128304e51ba08c08068d1cd8a
jradam <15231996+jradam@users.noreply.github.com>
2024-10-24 12:09:11 -0400

    added mobile styles to error notification

--- src/comps/error.ts
+++ src/comps/error.ts
@@ -2,9 +2,8 @@ customElements.define(
   'x-error',
   class extends HTMLElement {
     #handleError(error: ErrorEvent): void {
-      // TODO: Mobile styles
       this.className =
-        'box fixed bottom-8 left-8 z-50 rounded-lg bg-red-500 p-4 font-bold text-white'
+        'box fixed bottom-4 left-4 z-50 mr-8 rounded-lg bg-red-500 p-2 text-sm font-bold text-white sm:bottom-8 sm:left-8 sm:p-4 sm:text-base'
       this.innerHTML = 'Error: ' + error?.message || 'An unknown error occurred'
     }
 
